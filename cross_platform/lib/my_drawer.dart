1c38425a466e8945aac8e0221c8c6f26e80aee74
Andrew-Hanna <andrew.hannaa22@gmail.com>
2024-05-04 03:24:31 +0300

    drawer

--- cross_platform/lib/my_drawer.dart
+++ cross_platform/lib/my_drawer.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:cross_platform/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyDrawer extends StatelessWidget {
   MyDrawer({Key? key}) : super(key: key);
@@ -13,23 +16,154 @@ class MyDrawer extends StatelessWidget {
     return SizedBox(
       width: drawerWidth, // Control the width of the drawer
       child: Drawer(
+        backgroundColor: Colors.white,
         child: ListView(
           children: [
             ListTile(
-              title: Text('Login'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
+              title: Text(
+                'WELCOME TO OUR CLINIC',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 82, 191, 245),
+                  fontSize: 30,
+                  fontFamily: 'Serif',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 82, 191, 245),
+              thickness: 4,
+              indent: 20,
+              endIndent: 20,
             ),
             ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+              title: Text(
+                'My Profile',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              onTap: () {},
+              tileColor: const Color.fromARGB(255, 236, 233, 233),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Edit Profile',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.schedule,
+              ),
+              title: Text(
+                'My Appointments',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                'Services',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              tileColor: const Color.fromARGB(255, 236, 233, 233),
+            ),
+            ListTile(
+              minVerticalPadding: 0,
+              leading: Icon(
+                Icons.check_circle,
+              ),
+              title: Text(
+                'Dental checkup',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              minVerticalPadding: 0,
+              leading: Icon(
+                Icons.brush,
+              ),
+              title: Text(
+                'Teeth cleaning',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.remove_circle,
+              ),
+              minVerticalPadding: 0,
+              title: Text(
+                'Tooth extraction',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.healing,
+              ),
+              title: Text(
+                'Root canal treatment',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                FontAwesomeIcons.faceSmile,
+              ),
+              title: Text(
+                'Orthodontics',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                FontAwesomeIcons.teeth,
+              ),
+              title: Text(
+                'Teeth braces',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {},
             ),
           ],
         ),
